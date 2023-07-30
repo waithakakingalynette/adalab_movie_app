a3f7b63e1af0ba5f228415a86555f0303ddba44e
waithakakingalynette <warukiralynette@gmail.com>
2023-07-30 11:24:38 +0300

    added a limitation of movies I want to collect

--- src/Carousel/index.jsx
+++ src/Carousel/index.jsx
@@ -1,16 +1,36 @@
-import React, { Component } from 'react';  
-import ReactDOM from 'react-dom';  
-import "react-responsive-carousel/lib/styles/carousel.min.css"; 
-import { Carousel } from 'react-responsive-carousel';  
-   
-class DemoCarousel extends Component {  
-    render() {  
-        return (  
-            <Carousel>  
-                
-            </Carousel>  
-        );  
-    }  
-});  
-   
-ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));  
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+class DemoCarousel extends Component {
+  state = {
+    movies: []
+  };
+
+  componentDidMount() {
+    fetch('/api/movies')
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ movies: data.slice(0, 6) });
+      })
+      .catch((error) => console.error('Error fetching data:', error));
+  }
+
+  render() {
+    const { movies } = this.state;
+
+    return (
+      <Carousel>
+        {movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={movie.imageUrl} alt={movie.title} />
+            <p>{movie.title}</p>
+          </div>
+        ))}
+      </Carousel>
+    );
+  }
+}
+
+ReactDOM.render(<DemoCarousel />, document.querySelector('.demo-carousel'));
