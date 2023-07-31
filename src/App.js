001668662facd3f4191147df9e9b0139e79f75ea
waithakakingalynette <warukiralynette@gmail.com>
2023-07-31 17:58:19 +0300

    added slider in my app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import React from "react";
 import Navigation from './Navigation';
 import GetMovies from "./Components/getMovies";
 import Movies from "./Carousel";
-import Category from "./Components/Category";
-import MovieCategory from "./Components/Category/movieCategory";
 
 const App = () => {
   return (
       <div>
         <Navigation/>
         <Movies/>
-        <Category/>
-        <MovieCategory/>
         <GetMovies/> 
       </div>
   );
--- src/Navigation/index.jsx
+++ src/Navigation/index.jsx
@@ -1,72 +1,8 @@
-// import React, { useState } from "react";
-// import './Navigation.css';
-// import { searchMovies } from "../Utils/utilities";
-
-// const Navigation= () => {
-//   const [searchValue, setSearchValue] = useState("");
-//   const [searchResults, setSearchResults] = useState([]);
-//   const handleInput = (event) => {
-//     setSearchValue(event.target.value);
-//   };
-//   const handleSearch = async () => {
-//     const results = await searchMovies(searchValue);
-//     setSearchResults(results.results);
-//   };  
-  
-//   return (
-//     <div className="Navigation">
-//       <nav className="nav">
-//         <div>
-//           <h1 className="movie">
-//             M<span>oo</span>vie
-//           </h1>
-//         </div>
-//         <div className="search_button">
-//           <input
-//             value={searchValue}
-//             onChange={handleInput}
-//             type="text"
-//             placeholder="Search"
-//           />
-//           <button onClick={handleSearch} className="search-button">Search</button>
-//         </div>
-//         <div>
-//           <li className="home"><a href="">Home</a></li>
-//         </div>
-
-//         <div>
-//         <h4 className="list">My list</h4>
-//         </div>
-
-//         <button className="sign-in-button"> Sign In</button>
-//       </nav>    
-      
-//         {searchResults.length > 0 && (
-//         <div className="search-results">
-//           {searchResults.map((movie) => (
-//             <div key={movie.id} className="search-result">
-//               <img
-//                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-//                 alt={movie.title}
-//               />
-//               <h3>{movie.title}</h3>
-//               <p>{movie.overview}</p>
-//             </div>
-//           ))}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-// export default Navigation;
-
-
 import React, { useState } from "react";
-import { Link } from "react-router-dom"; // Import Link
 import './Navigation.css';
 import { searchMovies } from "../Utils/utilities";
 
-const Navigation = () => {
+const Navigation= () => {
   const [searchValue, setSearchValue] = useState("");
   const [searchResults, setSearchResults] = useState([]);
   const handleInput = (event) => {
@@ -76,10 +12,9 @@ const Navigation = () => {
     const results = await searchMovies(searchValue);
     setSearchResults(results.results);
   };  
-
+  
   return (
-
-        <div className="Navigation">
+    <div className="Navigation">
       <nav className="nav">
         <div>
           <h1 className="movie">
@@ -104,20 +39,19 @@ const Navigation = () => {
         </div>
 
         <button className="sign-in-button"> Sign In</button>
-      </nav> 
-
+      </nav>    
       
-      {searchResults.length > 0 && (
+        {searchResults.length > 0 && (
         <div className="search-results">
           {searchResults.map((movie) => (
-            <Link to={`/movie/${movie.id}`} key={movie.id} className="search-result">
+            <div key={movie.id} className="search-result">
               <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
               />
               <h3>{movie.title}</h3>
               <p>{movie.overview}</p>
-            </Link>
+            </div>
           ))}
         </div>
       )}
